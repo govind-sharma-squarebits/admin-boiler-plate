e511a1a5adfd80bb89cd48b6d4203f08d936154b
Govind Sharma <sharmagovind111085@gmail.com>
2026-03-24 16:47:21 +0530

    feat: Introduce Poppins font family and set it as the global default application font.

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 
 import { persistor, store } from "./redux";
+import "@/assets/font/Poppins/font.css";
 import "./index.css";
 
 import App from "./App";
